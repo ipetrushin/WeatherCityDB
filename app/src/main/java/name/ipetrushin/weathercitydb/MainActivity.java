0b3b90ce6abc928d0eaa19763aafa490b0d9a16a
Ivan Petrushin <ivan.kiel@gmail.com>
2019-03-28 13:28:43 +0800

    downgrade for Android Studio 3.0

--- app/src/main/java/name/ipetrushin/weathercitydb/MainActivity.java
+++ app/src/main/java/name/ipetrushin/weathercitydb/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         citiesList = findViewById(R.id.citiesList);
         dbHelper = new DBHelper(this);
         db = dbHelper.getWritableDatabase();
